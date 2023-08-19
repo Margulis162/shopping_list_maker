a59d9cf52cea709847cca812d832fb14a71449c3
Margulis162 <markmargulis@proton.me>
2023-08-18 21:04:50 -0400

    a function to collect input and populate cart list of objects has been created

--- shopping.js
+++ shopping.js
@@ -19,12 +19,18 @@ class Item {
 }
 
 const category_list =["vegitables", "dairy", "cosmetics", "meat & fish", "snacks"];
+const item_name = document.getElementById("item");
+const price = document.getElementById("price");
 const datalist = document.getElementById("cat_list");
+
+// button for adding items in the list
+add_btn = document.getElementById("add_item");
 
 /*___________________________f(x)___________________________*/
 
 // updates the shopping list 
 function updateList(){
+    list.innerHTML = "";
     for( i of cart){
         const position = list.appendChild(document.createElement('li'));
         position.textContent = `${i.name}______${i.price}$`;
@@ -42,15 +48,18 @@ function cat_data_creation(){
     }
 }
 
-
+// creates an item object and places it into the list
+function create_item(){
+   const item = new Item(item_name.value, price.value, datalist.value);
+   console.log(item);
+   cart.push(item);
+   updateList();
+}
  
 /*___________________________flow___________________________*/
 
-const apples =  new Item('apples', 4 , "vegie");
-apples.addToCart();
-const milk  = new Item('milk', 3 , 'dairy');
-milk.addToCart();
-updateList();
+
 
 cat_data_creation();
 
+add_btn.addEventListener(  "click", create_item);
