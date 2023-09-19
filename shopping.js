86c370945e71b9d429807b1c74866afbbb84610b
Margulis162 <markmargulis@proton.me>
2023-09-18 21:40:17 -0400

    reset

--- shopping.js
+++ shopping.js
@@ -1,81 +1,12 @@
 /*_____________________variables & classes___________________*/
-const list = document.getElementById("list");
-
-let cart =[];
-
-
-class Item {
-    name = "";
-    price = 1;
-    category = "";
-    constructor(name, price, category){
-        this.name = name;
-        this.price = price;
-        this.category = category;
-    }
-    addToCart(){
-      cart.push(this);
-    }
-}
-
-const category_list =["vegetables", "dairy", "cosmetics", "meat & fish", "snacks"];
-const item_name = document.getElementById("item");
-const price = document.getElementById("price");
-const datalist = document.getElementById("cat_list");
-
-const catOfItem = document.getElementById("category");
-
-// button for adding items in the list
-const add_btn = document.getElementById("add_item");
-
-// ref to total 
-const total = document.getElementById("total");
 
 
 
 /*___________________________f(x)___________________________*/
 
-// updates the shopping list 
-function updateList(){
-    list.innerHTML = "";
-    let total_price = 0;
-    for( i of cart){
-        
-        const position = list.appendChild(document.createElement('li'));
-        position.textContent = `${i.name}______${i.price}$`;
-        total_price += i.price;
-        console.log(total_price);
-    }
-    total.textContent = `Estimated total is ${total_price}`;
-}
 
-// creates a list of already recorded categories
-function cat_data_creation(){
-
-    for(i of category_list){
-        const cat = datalist.appendChild(document.createElement('option'));
-        
-        cat.textContent = `${i}`;
-    }
-}
-
-// creates an item object and places it into the list
-function create_item(){
-    if(item_name.value !==""  && price.value !== "" && catOfItem.value !== ""){
-        const item = new Item(item_name.value, Number(price.value), catOfItem.value);
-        console.log(item);
-        cart.push(item);
-        updateList();
-    }else{
-        alert('input missing')
-        console.log(catOfItem.value, item_name.value, price.value );
-    }
-}
  
 /*___________________________flow___________________________*/
 
 
 
-cat_data_creation();
-
-add_btn.addEventListener(  "click", create_item);
