a36a7d9d74bb12ca72e1af1829e5a7c5df7032a2
Margulis162 <markmargulis@proton.me>
2023-09-20 20:38:07 -0400

    close button adding function

--- shopping.js
+++ shopping.js
@@ -1,12 +1,21 @@
 /*_____________________variables & classes___________________*/
-
+let node_list =document.getElementsByTagName("li");
 
 
 /*___________________________f(x)___________________________*/
+function close_btn(){
+    for(const node of node_list){
+        let span = document.createElement("span");
 
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        node.appendChild(span);
+    }
+}
 
  
 /*___________________________flow___________________________*/
 
-
+close_btn();
 
